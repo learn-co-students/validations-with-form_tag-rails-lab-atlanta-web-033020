290b49e58b26df6bc1711001b42e73fdbaf18d8d
rpdecks <rpdecks@gmail.com>
2020-04-29 18:34:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :authors, only: [:new, :create, :edit, :update]
+  resource :posts, only: [:show, :new, :create, :edit, :update]
+  get "/authors/:id", to: "authors#show", as: "author"
 end
